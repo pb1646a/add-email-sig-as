55e24303033cccdd2e0c4196436adf3b9c52b083
Pari Baker <paribaker@Paris-MacBook-Pro.local>
2019-06-04 16:09:44 -0400

    working on dl from box

--- email_signatures/email_sig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# get profile
-currentuser=`stat -f "%Su" /dev/console`
-echo $currentuser
-sudo -u $(ls -l /dev/console | awk '{print $3}') osascript /Applications/email_signatures/set_email_signature.scpt
-echo $(ls -l /dev/console | awk '{print $3}')
--- remove_sig_script.sh
+++
@@ -1,7 +0,0 @@
-#/user/bin/perl
-
-UserName=$(ls -l /dev/console | awk '{print $3}')
-echo $UserName
-if [ -d /Users/$UserName/temp/ ]; then rm -Rf  /Users/$UserName/temp/ ; fi
-
-if [ -d /Applications/email_signatures ]; then rm -Rf  /Applications/email_signatures ; fi
